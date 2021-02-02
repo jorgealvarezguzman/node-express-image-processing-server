c6a7b7d78cbbdcc94573728b3cffe2f472019c58
Jorge Alfredo Alvarez Guzman <jorgealfredoalvarezguzman@Jorges-MacBook-Pro.local>
2021-02-02 17:33:28 -0500

    Completed module

--- api/src/router.js
+++ api/src/router.js
@@ -1,6 +1,7 @@
 const { Router } = require('express');
 const multer = require('multer');
 const path = require('path');
+const imageProcessor = require('./imageProcessor');
 
 const router = Router();
 const storage = multer.diskStorage({
@@ -13,13 +14,20 @@ const upload = multer({
 });
 const photoPath = path.resolve(__dirname, '../../client/photo-viewer.html');
 
-router.post('/upload', upload.single('photo'), (request, response) => {
+router.post('/upload', upload.single('photo'), async (request, response) => {
     if (request.fileValidationError) {
         return response.status(400), response.json({
             error: request.fileValidationError
         });
     }
     else {
+        try {
+            await imageProcessor(request.file.filename);
+        }
+        catch (error) {
+
+        }
+
         return response.status(201), response.json({
             success: true
         });
